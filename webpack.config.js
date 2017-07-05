f8b910eef5c05a323eb54481270d9149e04f0762
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-05 11:41:11 -0500

    configuration for reload and found routes

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,17 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'//for all the assets, this is to prevent to not find a route when reload
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: { // new property for exports object, this is to prevent to not find a route when reload
+    historyApiFallback: true
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
