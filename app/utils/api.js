3dc4bb68ee5f50033be7ea598268ef2310d3ca72
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-05 17:50:47 -0500

    Created small functions to decompose the API

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,67 @@
 var axios = require('axios');
 
+
+var id = "5f9b40f73c6b9d472637";
+var sec = "c154cb3ebe14ed52d6a6f2a792759dd9127bf325";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile (username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+  .then(function (user) {
+    return user.data;
+  });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore (profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData (player) {
+  return axios.all([//takes an array of promises
+    getProfile(player),
+    getRepos(player)
+  ]).then(function (data) {
+    var profile = data[0];
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players) {
+ return players.sort(function (a, b) {
+   return b.score - a.score; // the first is the highest score
+ })
+}
+
+
 module.exports = {
+  battle: function (players) {
+    return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError)
+  },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+'
       + language +
