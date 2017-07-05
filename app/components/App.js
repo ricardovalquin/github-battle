487ce8f1bc43faea138207ae8786c76bbec673ad
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-05 16:48:17 -0500

    Created the results component and the logic to update the view

--- app/components/App.js
+++ app/components/App.js
@@ -3,10 +3,12 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
-var Switch = ReactRouter.Switch;
+var Results = require('./Results');
 
 class App extends React.Component {
   render() { // everything the render return is the UI for the component
@@ -18,6 +20,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/battle" component={Battle}/>
+            <Route path="/battle/results" component={Results}/>
             <Route path="/popular" component={Popular}/>
             <Route render={function () {
               return <p>Not Found</p>
--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
 function PlayerPreview (props) {
   return (
@@ -106,6 +107,7 @@ class Battle extends React.Component {
   }
 
   render() {
+    var match = this.props.match;
     var playerOneName = this.state.playerOneName;
     var playerTwoName = this.state.playerTwoName;
     var playerOneImage = this.state.playerOneImage;
@@ -141,6 +143,14 @@ class Battle extends React.Component {
             onReset={this.handleReset}
           />}
         </div>
+        {playerOneImage && playerTwoImage &&
+          <Link className="button"
+                to={{pathname: match.url + '/results',
+                     search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName}}>
+            Battle!
+          </Link>
+
+        }
       </div>
     );
   }
