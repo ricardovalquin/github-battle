be49ca67aee48b887d31afa71f5bc34cfbe29d00
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-05 10:19:30 -0500

    Created a simple Nav component and added React router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,20 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
 
 class App extends React.Component {
   render() { // everything the render return is the UI for the component
     return (
-      <div className="container">
-        <h1>Say hello to my little friend!</h1>
-        <Popular/>
-      </div>
+      <Router>
+        <div className="container">
+          <h1>Say hello to my little friend!</h1>
+          <Nav/>
+          <Route path="/popular" component={Popular}/>
+        </div>
+      </Router>
     )
   }
 }
