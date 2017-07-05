5092fd0461bb538b17f5d6ad4578b350668036bc
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-05 14:31:45 -0500

    using the switch component for 404 requests (on FE)

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,7 @@ var Route = ReactRouter.Route;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component {
   render() { // everything the render return is the UI for the component
@@ -14,9 +15,14 @@ class App extends React.Component {
         <div className="container">
           <h1>Say hello to my little friend!</h1>
           <Nav/>
-          <Route exact path="/" component={Home}/>
-          <Route path="/battle" component={Battle}/>
-          <Route path="/popular" component={Popular}/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/battle" component={Battle}/>
+            <Route path="/popular" component={Popular}/>
+            <Route render={function () {
+              return <p>Not Found</p>
+            }}/>
+          </Switch>
         </div>
       </Router>
     )
